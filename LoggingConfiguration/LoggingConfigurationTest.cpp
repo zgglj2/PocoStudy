f03c0c5a9a44bb24557be28041af50291a83b305
glj <7909213@qq.com>
2017-06-22 18:17:55 +0800

    logging configuration

---
+++ LoggingConfiguration/LoggingConfigurationTest.cpp
@@ -0,0 +1,20 @@
+#include "Poco/Logger.h"
+#include <Poco/Util/PropertyFileConfiguration.h>
+#include <Poco/Util/LoggingConfigurator.h>
+#include <unistd.h>
+using Poco::Logger;
+
+int main(int argc, char **argv) {
+    Poco::AutoPtr<Poco::Util::PropertyFileConfiguration> pConf = new Poco::Util::PropertyFileConfiguration(
+            "../../LoggingConfiguration/LoggingConfigurationTest.properties");
+//    Poco::AutoPtr<Poco::Util::PropertyFileConfiguration> pConf = new Poco::Util::PropertyFileConfiguration(
+//            "../../Applications/ApplicationTestLog.properties");
+    Poco::Util::LoggingConfigurator log_configurator;
+    log_configurator.configure(pConf);
+
+    Logger &logger = Logger::root();
+    logger.information("This is an informational message");
+    logger.warning("This is a warning message");
+    sleep(5);
+    return 0;
+}
