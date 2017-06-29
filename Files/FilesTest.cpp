b1ab40216cde78bbd5fb64c4dfa568deda5a0a37
glj <7909213@qq.com>
2017-06-29 18:08:29 +0800

    xmind and File test

--- Files/FilesTest.cpp
+++ Files/FilesTest.cpp
@@ -1,21 +1,45 @@
-#include "Poco/DirectoryIterator.h"
+#include "Poco/File.h"
+#include "Poco/Path.h"
 #include <iostream>
-using Poco::DirectoryIterator;
-using Poco::Path;
 
-int main(int argc, char** argv)
-{
-    std::string cwd(Path::current());
-    DirectoryIterator it(cwd);
-    DirectoryIterator end;
-    while (it != end)
-    {
-        std::cout << it.name();
-        if (it->isFile())
-            std::cout << it->getSize();
-        std::cout << std::endl;
-        Path p(it.path());
-        ++it;
+using Poco::File;
+using Poco::Path;
+using namespace std;
+
+int main(int argc, char **argv) {
+    Path tmpPath(Path::temp());
+    tmpPath.pushDirectory("PocoFileSample");
+    File tmpDir(tmpPath);
+    tmpDir.createDirectories();
+    bool exists = tmpDir.exists();
+    cout << "tmpDir.exists() : " << exists << endl;
+    bool isFile = tmpDir.isFile();
+    cout << "tmpDir.isFile() : " << isFile << endl;
+    bool isDir = tmpDir.isDirectory();
+    cout << "tmpDir.isDirectory() : " << isDir << endl;
+    bool canRead = tmpDir.canRead();
+    cout << "tmpDir.canRead() : " << canRead << endl;
+    bool canWrite = tmpDir.canWrite();
+    cout << "tmpDir.canWrite() : " << canWrite << endl;
+    File tmpFile(Path(tmpPath, std::string("PocoFileSample.dat")));
+    if (tmpFile.exists() || tmpFile.createFile()) {
+        tmpFile.setSize(10000);
+        File tmpFile2(Path(tmpPath, std::string("PocoFileSample2.dat")));
+        cout << "tmpFile2.path() : " << tmpFile2.path() << endl;
+        tmpFile.copyTo(tmpFile2.path());
+        Poco::Timestamp now;
+        tmpFile.setLastModified(now);
+        tmpFile.setReadOnly();
+        canWrite = tmpFile.canWrite();
+        tmpFile.setWriteable();
+        canWrite = tmpFile.canWrite();
     }
+    std::vector<std::string> files;
+    tmpDir.list(files);
+    std::vector<std::string>::iterator it = files.begin();
+    for (; it != files.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
+//    tmpDir.remove(true);
     return 0;
 }
